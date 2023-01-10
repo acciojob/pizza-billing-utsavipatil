760bd25b34b96737caea09730b7f2a99d94cdb4a
Utsavi Patil <17bee076@nirmauni.ac.in>
2023-01-10 15:34:18 +0530

    Answer v1

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,7 +3,25 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
-        super(isVeg);
-        // your code goes here
+        super(isVeg); //parent's constructor
+        /*  All will work !!!
+
+            Option 1 due to Mehtod overriding these mentods are not defined in child class so default it will find in parent class
+            addExtraCheese();
+            addExtraToppings();
+
+            Option 2 call the local but mentod is not present in local it will automatically direct to find in parent
+            this.addExtraCheese();
+            this.addExtraToppings();
+
+            Option 3 direclty calling parent
+            super.addExtraCheese();
+            super.addExtraToppings();
+
+            Future option 1 & 2 should go because if we create local methods then thay will call first
+        */
+
+        addExtraCheese();
+        addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,13 +2,27 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    private int price; //Calculating final bill price
     private Boolean isVeg;
     private String bill;
-
+    private int cheesePrice, toppingsPrice , takeAwayPrice;
+    private boolean isCheeseAdded , isTakeAwayAdded , isToppingsAdded, isBillGenerated;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        this.cheesePrice = 80;
+        this.takeAwayPrice = 20;
+
+        if(isVeg){
+            this.price = 300;
+            this.toppingsPrice = 70;
+        }else{
+            this.price = 400;
+            this.toppingsPrice = 120;
+        }
+        this.isTakeAwayAdded = false;
+        this.isToppingsAdded = false;
+        this.isCheeseAdded = false;
+        this.bill = "Base Price Of The Pizza: " + this.price + "\n";
     }
 
     public int getPrice(){
@@ -16,19 +30,41 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(!isCheeseAdded){ //Extra Cheese Add only once
+            this.price = this.price + this.cheesePrice;
+            isCheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(!isToppingsAdded){ //Extra Toppings Add only once
+            this.price = this.price + this.toppingsPrice;
+            isToppingsAdded = true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(!isTakeAwayAdded){
+            this.price = this.price + this.takeAwayPrice;
+            isTakeAwayAdded = true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        if(!isBillGenerated){ //Bill generate single time
+            if(isCheeseAdded){
+                this.bill = this.bill + "Extra Cheese Added: " + this.cheesePrice + "\n";
+            }
+            if(isToppingsAdded){
+                this.bill = this.bill + "Extra Toppings Added: " + this.toppingsPrice + "\n";
+            }
+            if(isTakeAwayAdded){
+                this.bill = this.bill + "Paperbag Added: " + this.takeAwayPrice + "\n";
+            }
+            this.bill = this.bill + "Total Price: " + this.price + "\n";
+
+            isBillGenerated = true;
+        }
         return this.bill;
     }
 }
